54d9bdd4270e8c4d75bf62a33f59a68ddb4dbb1e
nazirmithu <nazirmithu22@gmail.com>
2023-06-09 22:42:42 +0600

    create user and signin method

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,16 +1,34 @@
 import { createContext, useState } from "react";
-
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    
+    const createUser = (email, password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    
+
 
     const authInfo = {
         user,
         loading,
-        
+        createUser,
+signIn,
+
     }
 
     return (
